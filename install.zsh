bc252a17a1e9428d5de5abef6a530dd83003ca20
Thomas Payne <tpayne.code@gmail.com>
2019-09-01 23:11:27 -0400

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -6,7 +6,6 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 
 ## install scm breeze
 git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
-~/.scm_breeze/install.sh
 
 ## install my custom prezto files
 for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/runcoms/^README.md(.N); do
